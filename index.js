a1e4bbb9689edd2411a3e23efb12a56baaef6cae
Repeatallright <levserg2003@gmail.com>
2023-12-30 00:25:33 +0300

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 let box = document.querySelector(".box");
-
+let deg = 0;
 let currentClass = "show-front";
 
 function changeSide() {
-  box.classList.remove(currentClass);
-  box.classList.add(getSide(currentClass));
-  currentClass = getSide(currentClass);
+  //   box.classList.remove(currentClass);
+  //   box.classList.add(getSide(currentClass));
+  //   currentClass = getSide(currentClass);
+  deg -= 90;
+  box.style.transform = `rotateY(${deg}deg)`;
+  console.log(box);
 }
 // set initial side
 
 box.addEventListener("click", changeSide);
 
-let cords = ["show-front", "show-right", "show-back", "show-left"];
+// let cords = ["show-front", "show-right", "show-back", "show-left"];
 
-function getSide(arg) {
-  if (cords.indexOf(arg) + 1 == cords.length) return cords[0];
-  else return cords[cords.indexOf(arg) + 1];
-}
+// function getSide(arg) {
+//   if (cords.indexOf(arg) + 1 == cords.length) return cords[0];
+//   else return cords[cords.indexOf(arg) + 1];
+// }
