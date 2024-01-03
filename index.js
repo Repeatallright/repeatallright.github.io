28efead293d447fb0d955884dddfb8ed43005f1c
Repeatallright <levserg2003@gmail.com>
2024-01-03 16:53:49 +0300

    designmode_2

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 let box = document.querySelector(".box");
 let deg = 0;
-let currentClass = "show-front";
 
-function changeSide() {
-  //   box.classList.remove(currentClass);
-  //   box.classList.add(getSide(currentClass));
-  //   currentClass = getSide(currentClass);
+box.addEventListener("click", () => {
   deg -= 90;
   box.style.transform = `rotateY(${deg}deg)`;
   console.log(box);
-}
-// set initial side
-
-box.addEventListener("click", changeSide);
-
-// let cords = ["show-front", "show-right", "show-back", "show-left"];
-
-// function getSide(arg) {
-//   if (cords.indexOf(arg) + 1 == cords.length) return cords[0];
-//   else return cords[cords.indexOf(arg) + 1];
-// }
+  if (deg % 180 == 0 && deg % 360 != 0)
+    box.style.boxShadow = "-5px 5px 5px -4px rgba(0,0,0,0.5)";
+  if (deg % 360 == 0)
+    box.style.boxShadow = "5px 5px 5px -4px rgba(0,0,0,0.5)";
+});
