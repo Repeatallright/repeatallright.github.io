72c2b821470ca4fcf2f4109de2c03361dfdf828b
Repeatallright <levserg2003@gmail.com>
2024-01-03 22:49:35 +0300

    designmode_2

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+let scene = document.querySelector(".scene");
 let box = document.querySelector(".box");
 let deg = 0;
 
-box.addEventListener("click", () => {
+scene.addEventListener("click", () => {
   deg -= 90;
   box.style.transform = `rotateY(${deg}deg)`;
   console.log(box);
   if (deg % 180 == 0 && deg % 360 != 0)
     box.style.boxShadow = "-5px 5px 5px -4px rgba(0,0,0,0.5)";
-  if (deg % 360 == 0)
-    box.style.boxShadow = "5px 5px 5px -4px rgba(0,0,0,0.5)";
+  if (deg % 360 == 0) box.style.boxShadow = "5px 5px 5px -4px rgba(0,0,0,0.5)";
 });
